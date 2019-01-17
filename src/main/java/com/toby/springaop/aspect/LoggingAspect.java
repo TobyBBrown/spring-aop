f0b51d770945e38bb365fbf4c9b10776582fc3aa
Toby Brown <tbrown08@dcs.bbk.ac.uk>
2019-01-17 16:57:23 +0000

    Create pointcut to use multiple advice methods on same methods

--- src/main/java/com/toby/springaop/aspect/LoggingAspect.java
+++ src/main/java/com/toby/springaop/aspect/LoggingAspect.java
@@ -2,12 +2,23 @@ package com.toby.springaop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-    @Before("execution(public * get*(..))")
+    @Before("allGetters()")
     public void LoggingAdvice() {
         System.out.println("Get method called.");
+    }
+
+    @Before("allGetters()")
+    public void secondAdvice() {
+        System.out.println("Second Advice");
+    }
+
+    @Pointcut("execution(public * get*(..))")
+    public void allGetters() {
+
     }
 }
